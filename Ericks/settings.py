5a64a38bcf9971987e5cf5f25af53692293691aa
rodgerswisdom <trulyhawona@gmail.com>
2024-07-15 20:00:10 +0000

    Database update

--- Ericks/settings.py
+++ Ericks/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-my4$fo99mqhay0)3hd@6q^$i*e0%a_86m_qgih9rbp_=a(!^=4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['aitools.myinsightiq.com']
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '3.128.156.10']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '3.128.156.10']
 
 
 # Application definition
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'Ericks.urls'
@@ -119,13 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = "/static/"
+# Note: Replace 'supersecure.codes' with your domain
+STATIC_ROOT = "/var/www/aitools.myinsightiq.com/static"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
