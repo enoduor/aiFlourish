0268d9a2ee78345746e04b99d2a29d3eb5c0778f
rodgerswisdom <130440506+rodgerswisdom@users.noreply.github.com>
2024-10-19 03:34:38 +0300

    feat: Setup tool service

--- tool_service/tools/models.py
+++ tool_service/tools/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Tool(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    website_link = models.URLField()
+    youtube_link = models.URLField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
---
+++ tool_service/tools/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Tool
+
+class ToolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tool
+        fields = ['id', 'name', 'description', 'website_link', 'youtube_link', 'category']
